b7009b20d60cf2a623e4a937679c6a79bc262a60
Peter Phillips <35266576+We1ghtless@users.noreply.github.com>
2021-11-15 10:19:09 +0000

    REST API initial commit

---
+++ backend/app.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+#Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#Init db
+db = SQLAlchemy(app)
+#Init ma
+ma = Marshmallow(app)
+
+# Selection Class/Model
+class Selection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+
+    def __init__(self, name):
+        self.name = name
+
+# Selection Schema
+class SelectionSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'name')
+
+#Init Schema
+selection_schema = SelectionSchema()
+selections_schema = SelectionSchema(many=True)
+
+# Create a Selection
+@app.route('/selection', methods=['POST'])
+def add_selection():
+    name = request.json['name']
+
+    new_selection = Selection(name)
+
+    db.session.add(new_selection)
+    db.session.commit()
+
+    return selection_schema.jsonify(new_selection)
+
+#Get All Selections
+@app.route('/selection', methods=['GET'])
+def get_selections():
+    all_selections = Selection.query.all()
+    result = selections_schema.dump(all_selections)
+    return jsonify(result.data)
+
+#Get Single Selection
+@app.route('/selection/<id>', methods=['GET'])
+def get_selection(id):
+    selection = Selection.query.get(id)
+    return selection_schema.jsonify(selection)
+
+# Create a Selection
+@app.route('/selection/<id>', methods=['PUT'])
+def update_selection(id):
+    selection = Selection.query.get(id)
+
+    name = request.json['name']
+
+    selection.name = name
+
+    db.session.commit()
+
+    return selection_schema.jsonify(selection)
+
+#Delete Selection
+@app.route('/selection/<id>', methods=['DELETE'])
+def delete_selection(id):
+    selection = Selection.query.get(id)
+
+    db.session.delete(selection)
+    db.session.commit()
+
+    return selection_schema.jsonify(selection)
+
+#Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
