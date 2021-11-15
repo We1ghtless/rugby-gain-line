b834ab06925a2c1089b883ea59d36d917ae3a7c1
Peter Phillips <35266576+We1ghtless@users.noreply.github.com>
2021-11-15 12:18:57 +0000

    Frontend initial build

---
+++ frontend/the-gain-line/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <Testing/>
+</template>
+
+<script>
+import Testing from './components/Testing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Testing
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
---
+++ frontend/the-gain-line/src/components/Testing.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="Testing">
+    <input type="button" value="submit" @click="onLoad()">
+    <p>{{this.info}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Testing',
+  data() {
+    return {
+      info: null
+    }
+  },
+  methods: {
+    onLoad() {
+
+      const axios = require('axios').default;
+      const apiKey = 'a5y3ft5z9a2ejvcym8zkh3xe'
+
+      axios({
+        method: 'get',
+        url: `https://api.sportradar.com/rugby-union/trial/v3/en/compeitions.json?api_key=${apiKey}`,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+      }
+      })
+      .then(res => (this.info = res))
+      .catch(err => console.error(err));
+    }
+  }
+}
+</script>
